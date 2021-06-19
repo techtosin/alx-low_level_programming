ab6ebe3c3494f5b30bea81c0e27c4cd6c5293aa7
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-19 11:59:34 -0700

    be

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Suceess)
  */
+
 int main(void)
 {
-printf("%"s, "with proper grammar, but the outcome is a piece of art,\n")
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
