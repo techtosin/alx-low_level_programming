06cf9ec03359508237a0ac0908f01df208e70899
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-19 14:28:53 -0700

    k

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", size of(char));
-printf("Size of an int: %ld byte(s)\n", size of(int));
-printf("Size of a long int: %ld byte(s)\n", size of(long int));
-printf("Size of a long long int: %ld byte(s)\n", size of(long long int));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", size of(float));
 return (0);
 }
