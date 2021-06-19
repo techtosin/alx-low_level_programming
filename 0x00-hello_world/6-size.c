31721af9bff8b2c15112a9f55386b6111c626fdf
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-19 13:21:49 -0700

    l

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %ld byte(s)\n", size of(char));
+printf("Size of an int: %ld byte(s)\n", size of(int));
+printf("Size of a long int: %ld byte(s)\n", size of(long int));
+printf("Size of a long long int: %ld byte(s)\n", size of(long long int));
+printf("Size of a float: %ld byte(s)\n", size of(float));
+return (0);
+}
+
