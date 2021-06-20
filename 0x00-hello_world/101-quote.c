674aee3ec4856484cdd3d7d88c571c959f963242
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-20 01:30:37 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+
 return (1);
 }
