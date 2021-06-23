bd9facf98690ba4293eecbc7fccfc72961038c67
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-23 05:38:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a line.
+ * print_alphabet - prints the alphabet in lowercase, followed by a anew line.
  */
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		
-		_putchar('\n');
+	for (letter = 'a'; letter <= "z"; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
