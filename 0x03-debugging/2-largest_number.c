41a396d38d54f03ac750b161065f587205d9ac6f
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-24 14:02:07 -0700

    1-main.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * * Return: largest number
+ * */
+
+int largest_number(int a, int b, int c)
+{
+	if (c >= b)
+	{
+		if (c >= a)
+		{
+			return (c);
+		}
+		else
+		{
+			return (a);
+		}
+	}
+	else
+	{
+		if (b >= a)
+		{
+			return (b);
+		}
+		else
+		{
+			return (a);
+		}
+	}
+}
