2cae72c3f080803e6e23b32edc5ac0a9c374c533
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-25 10:40:52 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -8,18 +9,20 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming  :(\n");
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 	/*
-	*while (i < 10)i
-	*{
-	*putchar(i);
-	*}
-	*/
-	printf("Infinite loop avoided! \\o/\n"));
+	 * while (i < 10)
+	 *{
+	 putchar(i);
+	 *}
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
-© 2021 GitHub, Inc.
-© 2021 GitHub, Inc.
 
-i
+
+
+
