efc205ebf496aa1bbd909387120e2d44b4f8d918
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-28 15:46:07 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,15 +1,15 @@
-nclude "holberton.h"
+#include "holberton.h"
 
 /**
- *  * main - check the code for ALX School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code for ALX School students .
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    char *str;
+	char *str;
 
-	        str = "0123456789";
-		    puts_half(str);
-		        return (0);
+	str = "0123456789";
+	puts_half(str);
+	return (0);
 }
