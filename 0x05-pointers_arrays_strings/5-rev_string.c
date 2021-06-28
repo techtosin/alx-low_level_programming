c5b7f355f40de4daee0a40759c22baf1596f3a58
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-28 13:37:21 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,22 @@
-tring - reverses given string
+/**
+ * rev_string - reverses given string
  *
- *  * @s: string to reverse
- *   *
- *    * Return: always void
- *     */
- void rev_string(char *s)
+ * @s: string to reverse
+ *
+ * Return: always void
+ */
+void rev_string(char *s)
 {
-		char *end = s;
-			char store;
+	char *end = s;
+	char store;
 
-				while (*end)
-							end++; /* set pos of end pointer to point to last element */
-					end = end - 1; /* set end pos to end -1 b/c end is really '\0' */
-						while (s < end) /* stop once we get to last address */
-								{
-											store = *s; /* set store ch to s[currentPos] */
-													*s++ = *end; /* set position of s = pos end, and increment s */
-															*end-- = store; /* set pos of end to stored char and increment */
-																}
+	while (*end)
+		end++;
+	end = end - 1;
+	while (s < end)
+{
+	store = *s;
+	*s++ = *end;
+	*end-- = store;
+}
 }
