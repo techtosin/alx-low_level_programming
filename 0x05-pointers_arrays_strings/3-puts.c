7cf251834c8563fc8b8876f78b73da49b1583fd0
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-06-28 08:19:09 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+nclude "holberton.h"
+/**
+ *  * _puts - puts string to stdout
+ *   *
+ *    * @str: string to print to stdout
+ *     *
+ *      * Return: always void
+ *       */
+void _puts(char *str)
+{
+		while (*str)
+					_putchar(*str++);
+			_putchar('\n');
+}
+
