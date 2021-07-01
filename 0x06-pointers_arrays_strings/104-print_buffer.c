ef2bfd785bc91b398d1c37e3d4e4afff2013ad43
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-01 13:18:57 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,60 @@
-nclude <stdio.h>
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- *  * print_buffer - Print the entire buffer w/ certain conditions
- *   * @b: The buffer to print
- *    * @size: The size of the buffer
- *     */
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf(" ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+			if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+				putchar(c[l * 10 + k]);
+			else
+				putchar('.')
+}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-		int i, j;
-
-			i = 0;
-				if (size <= 0)
-							putchar('\n');
-					else
-							{
-										while (i < size)
-													{
-																	printf("%08x: ", i);
-																				j = 0;
-																							while (j < 10)
-																											{
-																																if (j % 2 == 0 && j > 0)
-																																						printf(" ");
-																																				if (j + i > size - 1)
-																																										printf("  ");
-																																								else
-																																														printf("%.2x", b[j + i]);
-																																												j++;
-																																															}
-																										putchar(' ');
-																													j = 0;
-																																while (j < 10)
-																																				{
-																																									if (j + i > size - 1)
-																																															break;
-																																													if (b[j + i] >= ' ' && b[j + i] <= '~')
-																																																			putchar(b[j + i]);
-																																																	else
-																																																							putchar('.');
-																																																					j++;
-																																																								}
-																																			putchar('\n');
-																																						i += 10;
-																																								}
-											}
+	int i;
+	
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+			print_line(b, size % 10 - 1, i);
+	}
+	putchar('\n');
 }
-© 
+if (size == 0)
+	putchar('\n');
+	}
