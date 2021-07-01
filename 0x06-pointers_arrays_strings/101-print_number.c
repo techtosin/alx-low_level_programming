25132b7cf5cd93e1d2d5d34c165bcc930daddddd
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-01 05:13:02 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,14 +3,14 @@
 /**
  * print_number - prints an integer.
  * @n: integer.
- * Return: nothing.
+ * Return: nothing
  */
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-			n = -n;
+		n = -n;
 	}
 	if (n / 10)
 		print_number(n / 10);
