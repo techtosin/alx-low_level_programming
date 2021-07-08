dad87736f9bcf57a253c12d8300ced7df73e72d3
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-08 06:51:30 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,12 @@
-nt check_palindrome_recursive(char *start, char *end);
+int check_palindrome_recursive(char *start, char *end);
 int _strlen_recursion(char *s);
 /**
- *  * is_palindrome - checks if string is palindrome
- *   *
- *    * @s: string to check
- *     *
- *      * Return: 1 if palindrome, 0 otherwise
- *       */
+ * is_palindrome - checks if string is palindrome
+ *
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
 	char *end;
@@ -19,13 +19,13 @@ int is_palindrome(char *s)
 	return (check_palindrome_recursive(s, end));
 }
 /**
- *  * check_palindrome_recursive - checks if string is palindrome
- *   *
- *    * @start: starting point of string to check
- *     * @end: last valid character in string before null byte
- *      *
- *       * Return: 1 if palindrome, 0 if failure (not palindrome)
- *        */
+ * check_palindrome_recursive - checks if string is palindrome
+ *
+ * @start: starting point of string to check
+ * @end: last valid character in string before null byte
+ *
+ * Return: 1 if palindrome, 0 if failure (not palindrome)
+ */
 int check_palindrome_recursive(char *start, char *end)
 {
 	if (*start != *end)
@@ -33,3 +33,17 @@ int check_palindrome_recursive(char *start, char *end)
 	if (start > end) /* checked all chars up to middle */
 		return (1);
 	return (check_palindrome_recursive(start + 1, end - 1));
+}
+/**
+ * _strlen_recursion - gets strlen of s via recursive algorithm
+ *
+ * @s: string to check length of
+ *
+ * Return: int containing length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
