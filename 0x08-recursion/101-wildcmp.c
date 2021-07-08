8b2d3243880c97b57a1ea600fb438084ddd6f011
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-08 11:10:43 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,35 +1,49 @@
-nt check_palindrome_recursive(char *start, char *end);
-int _strlen_recursion(char *s);
+int recursive_check(char *s1, char *s2, int wildC);
 /**
- *  * is_palindrome - checks if string is palindrome
- *   *
- *    * @s: string to check
- *     *
- *      * Return: 1 if palindrome, 0 otherwise
- *       */
-int is_palindrome(char *s)
+ * wildcmp - compares two strings, supports wildcard '*'
+ *
+ * @s1: string 1 to compare to s2
+ * @s2: string 2 to compare to s1
+ *
+ * Return: 1 if identical, 0 otherwise, -1 for error
+ */
+int wildcmp(char *s1, char *s2)
 {
-	char *end;
-	int sLength;
-
-	if (!*s) /* empty string */
-		return (1);
-	sLength = _strlen_recursion(s);
-	end = (s + (sLength - 1)); /* set end to char before null byte */
-	return (check_palindrome_recursive(s, end));
+	return (recursive_check(s1, s2, 0));
 }
 /**
- *  * check_palindrome_recursive - checks if string is palindrome
- *   *
- *    * @start: starting point of string to check
- *     * @end: last valid character in string before null byte
- *      *
- *       * Return: 1 if palindrome, 0 if failure (not palindrome)
- *        */
-int check_palindrome_recursive(char *start, char *end)
+ * recursive_check - checks for wild cards recursively
+ *
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
+ *
+ * Return: 1 if identical, 0 for not, -1 for error
+ */
+int recursive_check(char *s1, char *s2, int wildC)
 {
-	if (*start != *end)
-		return (0);
-	if (start > end) /* checked all chars up to middle */
+	if (*s1 == '\0' && *s2 == '\0')
+	{
 		return (1);
-	return (check_palindrome_recursive(start + 1, end - 1));
+	}
+	if (*s2 == '*')
+		return (recursive_check(s1, s2 + 1, ++wildC));
+	else if (*s1 != *s2 && wildC > 0) /* skip to next rel char */
+	{
+		if (!*s1)
+			return (0);
+		return (recursive_check(s1 + 1, s2, wildC));
+	}
+	else if (*s1 == *s2 && wildC == 0) /* char match, next char for each str */
+		return (recursive_check(s1 + 1, s2 + 1, 0));
+	else if (*s1 == *s2 && wildC > 0)
+	{
+		if (!recursive_check(s1 + 1, s2 + 1, 0))
+			return (recursive_check(s1 + 1, s2, wildC));
+		return (recursive_check(s1 + 1, s2 + 1, wildC));
+	}
+	else if (*s1 != *s2 && wildC == 0) /* non matching and no wildC */
+		return (0);
+	return (-1); /* something unexpected occured, return -1 for error */
+}
+
