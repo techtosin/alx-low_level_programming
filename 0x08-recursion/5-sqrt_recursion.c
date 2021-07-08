31661e1fc50b6b782300224c166864f0047db280
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-08 05:59:19 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,12 @@
-nt _find_sqrt(int prev, int find);
+#include "holberton.h"
+int _find_sqrt(int prev, int find);
 /**
- *  * _sqrt_recursion - finds square root of n, recursively
- *   *
- *    * @n: int to find sqroot from
- *     *
- *      * Return: sqroot, -1 if no natural sqroot for n
- *       */
+ * _sqrt_recursion - finds square root of n, recursively
+ *
+ * @n: int to find sqroot from
+ *
+ * Return: sqroot, -1 if no natural sqroot for n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n <= 0) /* error case */
@@ -13,13 +14,13 @@ int _sqrt_recursion(int n)
 	return (_find_sqrt(1, n));
 }
 /**
- *  * _find_sqrt - finds square root recursively, needs prev param
- *   *
- *    * @prev: previous result of function
- *     * @find: constant int to find sq root for
- *      *
- *       * Return: square root of find, or -1 if not found
- *        */
+ * _find_sqrt - finds square root recursively, needs prev param
+ *
+ * @prev: previous result of function
+ * @find: constant int to find sq root for
+ *
+ * Return: square root of find, or -1 if not found
+ */
 int _find_sqrt(int prev, int find)
 {
 	if (prev > find) /* didn't find a nr, there was remainder */
@@ -28,4 +29,4 @@ int _find_sqrt(int prev, int find)
 		return (prev);
 	return (_find_sqrt(prev + 1, find)); /* not super efficient */
 }
-©
+
