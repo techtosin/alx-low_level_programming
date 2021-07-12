db76ed415b1355fd4a74654d6e915a8f37b7d2d2
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-12 11:25:38 -0700

    1-args.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+nclude <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  * main - multiplies two arguments and prints result
+ *   * @argc: argument count
+ *    * @argv: argument vector
+ *     * Return: 0 if no errors
+ *      */
+
+int main(int argc, char *argv[])
+{
+	int i, first_num, sec_num, product;
+	if (argc != 3)
+	{
+		return (printf("Error\n"), 1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		first_num = atoi(argv[1]);
+		sec_num = atoi(argv[2]);
+	}
+	product = first_num *sec_num;
+	printf("%d\n", product);
+	return (0);
+}
+© 2021 GitHub, Inc.
