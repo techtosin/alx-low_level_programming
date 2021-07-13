d985d7b233d61e6c7c5bd56653e196df3d13de6b
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-13 12:59:12 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,39 +1,34 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * _strdup - Return pointer to a new string that duplicates given string,
- * allocates men w/ malloc
- * @str: String to duplicate
- *
- * Return: Pointer to a new string, NULL if failed to make memory
+ * *_strdup - return a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: string
+ * Return: 0
  */
 
 char *_strdup(char *str)
 {
-ar *_strdup(char *str)
+int i = 0, size = 0;
+char *m;
+
+if (str == NULL)
+return (NULL);
+
+for (; str[size] != '\0'; size++)
+;
+
+/*+1 on the size puts the end of string character*/
+m = malloc(size * sizeof(*str) + 1);
+
+if (m == 0)
+	return (NULL);
+else
 {
-	char *nstr;
-	unsigned int i, len;
-
-	if (str == NULL)
-		return (NULL);
-	i = len = 0;
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	len++;
-	nstr = malloc(len * sizeof(*str));
-	if (nstr == NULL)
-		return (NULL);
-	while (i <= len)
-	{
-		nstr[i] = str[i];
-		i++;
-	}
-	return (nstr);
+for (; i < size; i++)
+m[i] = str[i];
 }
-
-
+return (m);
+}
+©
