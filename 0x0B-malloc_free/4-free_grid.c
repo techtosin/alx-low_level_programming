1ce068c329c45cb195ca28a7c0d29b959c52c66a
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-13 07:01:19 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,24 +1,24 @@
-nclude "holberton.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- *  * free_grid - frees a 2 dimensional grid
- *   * @grid: the address of the two dimensional grid
- *    * @height: height of the grid
- *     *
- *      * Description: print_chessboard
- *       * Return: Nothing.
- *        */
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Description: print_chessboard
+ * Return: Nothing.
+ */
 
 void free_grid(int **grid, int height)
 {
-		int i;
+	int i;
 
-			for (i = 0; i < height; i++)
-					{
-								free(grid[i]);
-									}
-				free(grid);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
 
