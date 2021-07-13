e6dc8c86df4806d6d56ebfb93e46e72457531554
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-13 06:42:55 -0700

    2-str_concat.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: input size
- * @c: input character
- *
- * Return: a pointer to the array, or NULL if it fails
- */
+ *  * create_array - creates an array of chars,
+ *   * and initializes it with a specific char
+ *    * @size: input size
+ *     * @c: input character
+ *      *
+ *       * Return: a pointer to the array, or NULL if it fails
+ *        */
 
 char *create_array(unsigned int size, char c)
 {
@@ -19,13 +19,13 @@ char *create_array(unsigned int size, char c)
 	arr = (char *) malloc(size);
 	if (size == 0 || arr == NULL)
 	{
-		return (NULL);
+			return (NULL);
+		}
+	for (i = 0; i < size; i++)
+	{
+			arr[i] = c;
+		}
+	return (arr);
 }
-for (i = 0; i < size; i++)
-{
-
-	arr[i] = c;
-}
-return (arr);
-}
+© 2021 GitHub, Inc.}
 
