51864344633bfc07392c3e68639d1ded1b6d8e84
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-15 07:49:57 -0700

    100-realloc.c

--- 0x0C-more-malloc_free/100-realloc.c
+++ 0x0C-more-malloc_free/100-realloc.c
@@ -1,11 +1,11 @@
-nclude "holberton.h"
+#include "holberton.h"
 
 /**
- *  * _memcpy - copy memory data from src to dest
- *   * @dest: memory destination
- *    * @src: memory source
- *     * @n: size of new memory
- *      */
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
+ */
 void _memcpy(void *dest, void *src, size_t n)
 {
 	size_t i;
@@ -17,17 +17,18 @@ void _memcpy(void *dest, void *src, size_t n)
 }
 
 /**
- *  * *_realloc - reallocates a memory block using malloc and free
- *   * @ptr: array length
- *    * @old_size: size of old memory
- *     * @new_size: size of new memory
- *      * Return: pointer to new memory
- *       */
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: array length
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: pointer to new memory
+ */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
 	void *newPtr;
+
 	if (new_size == 0)
 	{
 		if (ptr != NULL)
@@ -55,4 +56,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	return (0);
 }
-© 2021 GitHub, Inc.
+
