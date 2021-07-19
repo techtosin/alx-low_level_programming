0bad4e071232acf832ff583fdf2aec43e7884ac2
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-19 06:21:20 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,15 @@
-nclude "dog.h"
+#include "dog.h"
 
 /**
- *  * free_dog - frees dogs
- *   * @d: pointer to dog
- *    */
+ * free_dog - frees dogs
+ * @d: pointer to dog
+ */
 
 void free_dog(dog_t *d)
 {
-		if (!d)
-					return;
-			free(d->name);
-				free(d->owner);
-					free(d);
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
