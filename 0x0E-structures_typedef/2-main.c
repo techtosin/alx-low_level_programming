25e1b5ce181f14de94229bde88fbde27ff1ee0cd
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-07-19 05:40:40 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,18 +1,18 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
- *  * main - check the code for ALX School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    struct dog my_dog;
+	struct dog my_dog;
 
-	        my_dog.name = "Poppy";
-		    my_dog.age = 3.5;
-		        my_dog.owner = "Bob";
-			    print_dog(&my_dog);
-			        return (0);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
 }
