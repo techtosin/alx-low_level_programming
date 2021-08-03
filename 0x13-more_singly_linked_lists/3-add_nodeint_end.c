0bdc41fbd75625046b0a001df3a780339c7e402c
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-03 03:47:53 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,13 +5,13 @@
  * @head: a double pointer to the starting node of the linked list
  * @n: the data to be stored in the new element
  *
- * Return: address of the new element
+ * Return: a pointer to the newly created node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *current;
 
-	new_node = malloc(sizeof(istint_t));
+	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
