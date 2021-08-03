727dd452a29e39b5e88998cb1647c79bd06cfe7e
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-03 03:31:36 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,37 +1,34 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * add_nodeint_end - adds a new node at theend of a listint_t list.
- *   * @head: head node
- *    * @n: number of input nodes
- *     * Return: address of the new element
- *      */
-
+ * add_nodeint_end - adds a new node at theend of a listint_t list.
+ * @head: a double pointer to the starting node of the linked list
+ * @n: the data to be stored in the new element
+ *
+ * Return: address of the new element
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-		listint_t *new_node = malloc(sizeof(listint_t));
-			listint_t *current_node;
+	listint_t *new_node, *current;
 
-				if (head == NULL || new_node == NULL)
-						{
-									return (NULL);
-										}
-					new_node->n = n;
-						new_node->next = NULL;
+	new_node = malloc(sizeof(istint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 
-							if ((*head) == NULL)
-									{
-												(*head) = new_node;
-													}
-								else
-										{
-													current_node = (*head);
-															while (current_node->next != NULL)
-																		{
-																						current_node = current_node->next;
-																								}
-																	current_node->next = new_node;
-																		}
-									return (new_node);
+	if (*head)
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+
+		current->next = new_node;
+	}
+	else
+	{
+		*head = new_node;
+		return (*head);
+	}
+	return (current);
 }
-© 2021 GitHub, Inc.
