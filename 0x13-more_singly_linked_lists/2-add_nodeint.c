ab2eb225a8200e7774c9597396bce9d2ab6dace0
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-03 03:13:13 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,28 +1,24 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * add_nodeint - adds a new node at the beginning
- *   * of a listint_t list
- *    * @head: head node
- *     * @n: number of input value
- *      * Return: address of the new element
- *       */
-
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: the starting node
+ * @n: the data to be stored in a node.
+ *
+ * Return: the address of the new element
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-		listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node;
 
-			if (!head || !new_node)
-					{
-								return (NULL);
-									}
-				new_node->n = n;
-					new_node->next = NULL;
-						if (*head)
-								{
-											new_node->next = *head;
-												}
-							*head = new_node;
-								return (new_node);
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
-
