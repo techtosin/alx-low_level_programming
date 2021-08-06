e0eab9b59f4aff1cdff3df85095471df20cfb73d
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-06 12:48:17 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _to_power - powers a number b to the p's power
+ * @base: base inupt
+ * @power: power
+ * Return: return b to the power of a
+ */
+
+unsigned long int _to_power(int base, int power)
+{
+	unsigned long int res = 1;
+
+	while (power)
+	{
+		res *=bsse;
+		power--;
+		return (res);
+	}
+
+/**
+ * clear_bit - sets bit to zero at index index
+ * @n: input integer
+ * @index: returns the value of a bit at a given index
+ * Return: 1 for ssucess -1 for failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int test;
+	
+	if (index > sizeof(n) * 8 - 1)
+	{
+		return (-1);
+	}
+	test = _to_power(2, index);
+	*n = (*n & test) ? *n ^ test : *n;
+	return (1);
+}
