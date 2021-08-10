6f4fb86c5be9cf9de0ed8a07fc3ad0c471526e2e
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-10 07:08:17 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,18 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <elf.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
 
-int append_text_to_file(const char *filename, char *text_content);
-int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *file);
+void close_file(int);
 
 #endif
+f
