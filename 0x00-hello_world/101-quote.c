c3dbb7f5e0477fb42fd1af883d33ba89082b0028
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-22 11:08:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
