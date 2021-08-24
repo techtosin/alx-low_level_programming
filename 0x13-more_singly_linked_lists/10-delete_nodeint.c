de87206f5cd0677a158243f9d1205a5f65d99131
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-24 05:48:38 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,42 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a
- * listint_t linked list.
- * @head: a double pointer to the starting node of listint_t list
- * @index: the position of the element of the listint_t list to be
- * deleted.
- *
+ * delete_nodeint_at_index - prints elements of a list_t list
+ * @head: head node
+ * @index: the index of the node that should be deleted
  * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int n = 0;
-	listint_t *temp, *next;
+	{
+		listint_t *curr_node;
+		listint_t *next;
 
-	if (!*head)
-		return (-1);
+		unsigned int i;
 
-	temp = *head
-
+		if (!head || !(*head))
+		{
+			return (-1);
+		}
+		curr_node = (*head);
 		if (!index)
 		{
-			*head = temp->next;
-			free(temp);
+			(*head) = curr_node->next;
+			free(curr_node);
 			return (1);
 		}
-
-	while (temp && (n++ < (index - 1)))
-		temp = temp->next;
-
-	if (!temp || !temp->next)
-		return (-1);
-
-	next = temp->next->next;
-	free(temp->next);
-	temp->next = next;
-
-	return (1);
+		for (i = 0; curr_node && i < index - 1; i++)
+		{
+			curr_node = curr_node->next;
+		}
+		if (!curr_node || !(curr_node->next))
+		{
+			return (-1);
+		}
+		next = curr_node->next->next;
+		free(curr_node->next);
+		curr_node->next = next;
+		return (1);
+	}
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,45 @@
 #include "lists.h"
 
-/*
- * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: the pointer to the starting node of the listint_t list
- * @idx: the position where the new node is to be inserted
- * @n: the data to be stored in the new node
+/**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: the address of the new node, or NULL if it failed
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *temp = *head;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
-	if (!idx)
+	if (idx == 0)
 	{
-		new->next = temp;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
 
-	while (i++ < (idx - 1))
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (!temp || !(temp->next))
-			return (NULL);
-
-		temp = temp->next;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
 
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new);
+	return (NULL);
 }
