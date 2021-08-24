5a409bc0816d7e05069e2c453338d417545403f0
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-24 06:13:24 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,24 +7,22 @@
  * Return: 1 for success
  */
 
-int main(argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+register int a, b;
+int (*fptr)(int, int);
 
-				if (argc != 4)
-				{
-					printf("Error\n"), exit(98);
-				}
-				fptr = get_op_func(argv[2]);
-				if (!fptr)
-				{
-					printf("Error\n"), exit(99);
-				}
-				a = atoi(argv[1]);
-				b = atoi(argv[3]);
-				printf("%i\n", fptr(a, b));
-				return (1);
+if (argc != 4)
+{
+printf("Error\n"), exit(98);
 }
-
-
+fptr = get_op_func(argv[2]);
+if (!fptr)
+{
+printf("Error\n"), exit(99);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+printf("%i\n", fptr(a, b));
+return (1);
+}
