352ad2aa1d742f2d859ad92f88e9d1b6a6ad7c91
Alayande Oluwatosin <sewnthimble@gmail.com>
2021-08-24 05:37:04 -0700

    100-elf_header.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- *  * create_array - creates an array of chars,
- *   * and initializes it with a specific char
- *    * @size: input size
- *     * @c: input character
- *      *
- *       * Return: a pointer to the array, or NULL if it fails
- *        */
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: input size
+ * @c: input character
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
 
 char *create_array(unsigned int size, char c)
 {
--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,12 +1,12 @@
-nclude "holberton.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- *  * main - check the code for ALX School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	    char *s;
